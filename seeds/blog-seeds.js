1b3ccc7f2f5ebc760e449f16ca7bc8ac271c5850
jcg0 <jg.awsprod@gmail.com>
2023-02-21 17:00:13 -0500

    added more blog seeds

--- seeds/blog-seeds.js
+++ seeds/blog-seeds.js
@@ -7,6 +7,42 @@ const blogData = [
     "blog_title": 'LOTR',
     "creation_date": 2023-2-14
   },
+  {
+    "user_id": 1,
+    "blog_content": "Management fingers Tom wise fouler? Squirrel finest devouring barred Ettenmoors descend summer overflowing as bloom childhood.",
+    "blog_title": 'LOTR',
+    "creation_date": 2023-2-14
+  },
+  {
+    "user_id": 1,
+    "blog_content": "Management fingers Tom wise fouler? Squirrel finest devouring barred Ettenmoors descend summer overflowing as bloom childhood.",
+    "blog_title": 'LOTR',
+    "creation_date": 2023-2-14
+  },
+  {
+    "user_id": 1,
+    "blog_content": "Management fingers Tom wise fouler? Squirrel finest devouring barred Ettenmoors descend summer overflowing as bloom childhood.",
+    "blog_title": 'LOTR',
+    "creation_date": 2023-2-14
+  },
+  {
+    "user_id": 1,
+    "blog_content": "Management fingers Tom wise fouler? Squirrel finest devouring barred Ettenmoors descend summer overflowing as bloom childhood.",
+    "blog_title": 'LOTR',
+    "creation_date": 2023-2-14
+  },
+  {
+    "user_id": 1,
+    "blog_content": "Management fingers Tom wise fouler? Squirrel finest devouring barred Ettenmoors descend summer overflowing as bloom childhood.",
+    "blog_title": 'LOTR',
+    "creation_date": 2023-2-14
+  },
+  {
+    "user_id": 1,
+    "blog_content": "Management fingers Tom wise fouler? Squirrel finest devouring barred Ettenmoors descend summer overflowing as bloom childhood.",
+    "blog_title": 'LOTR',
+    "creation_date": 2023-2-14
+  },
 ]
 
 const seedBlogs = () => Blog.bulkCreate(blogData);
